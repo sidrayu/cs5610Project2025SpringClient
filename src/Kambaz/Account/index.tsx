07f934eb1c7edaec336369efaeb866dd2d228cbe
KHuang00 <Kathiehuang0@gmail.com>
2025-04-12 18:19:45 -0400

    add team info

--- src/Kambaz/Account/index.tsx
+++ src/Kambaz/Account/index.tsx
@@ -22,6 +22,42 @@ export default function Account() {
         <Route path="/Profile" element={<Profile />} />
         <Route path="/Signup" element={<Signup />} />
       </Routes>
+
+              <br/><br/><br/>
+
+      <h3>Team Info</h3>
+      <div style={{ marginLeft: "1rem", lineHeight: "1.8" }}>
+
+          <p><strong>CS5610 Group Project GitHub Links</strong><br />
+          React.js: <a href="https://github.com/sidrayu/cs5610Project2025SpringClient" target="_blank" rel="noreferrer">github.com/team/react-project</a><br />
+          Node.js: <a href="https://github.com/sidrayu/cs5610Project2025SpringServer" target="_blank" rel="noreferrer">github.com/team/node-project</a></p>
+
+          <p><strong>Chang Yu</strong><br />
+              Email: email<br />
+              GitHub: <a href="https://github.com/sidrayu" target="_blank" rel="noreferrer">github.com/Chang Yu</a></p>
+
+
+          <p><strong>Katharine Huang</strong><br />
+               Email: huang.kat@northeastern.edu<br />
+               GitHub: <a href="https://github.com/KHuang00" target="_blank" rel="noreferrer">github.com/katharinehuang</a></p>
+
+
+          <p><strong>NAME</strong><br />
+              Email: email<br />
+              GitHub: <a href="https://XX" target="_blank" rel="noreferrer">github.com/NAME</a></p>
+
+          <p><strong>NAME</strong><br />
+              Email: email<br />
+              GitHub: <a href="https://XX" target="_blank" rel="noreferrer">github.com/NAME</a></p>
+
+          <p><strong>NAME </strong><br />
+              Email: email<br />
+              GitHub: <a href="https://XXX" target="_blank" rel="noreferrer">github.com/NAME</a></p>
+
+
+
+      </div>
+
       </td>
         </tr>
       </table>
